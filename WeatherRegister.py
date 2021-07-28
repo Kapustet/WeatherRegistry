a666415f59912d1944f22634c8be5a1ed87d7794
wl <Wojciech.Langowski@gmx.com>
2021-07-28 13:08:15 +0200

    Finished Functionality

--- WeatherRegister.py
+++ WeatherRegister.py
@@ -2,35 +2,56 @@ import datetime
 import time
 import requests
 from apiKey import apiKey
+from dbconfig import dbconfig,_SQL
+from DatabaseContextManager import UseDatabase
 
+class DataGatherer:
+    def __init__(self,city,units,apiKey):
+        self.cityName = city
+        self.units = units
+        self.apiKey = apiKey
+    
+    def gather_data(self)->dict:
+        fullAddress = f'http://api.openweathermap.org/data/2.5/weather?q={self.cityName}&units={self.units}&appid={self.apiKey}'
+        data = requests.get(fullAddress).json()
+        return data
 
-cityName = 'Tczew'
-units = 'metric'
-fullAddress = f'http://api.openweathermap.org/data/2.5/weather?q={cityName}&units={units}&appid={apiKey}'
+class Weather:
+    def __init__(self,data:dict):
+        self.description = data['weather'][0]['description']
+        self.currentTemp = data['main']['temp']
+        self.feelsLikeTemp = data['main']['feels_like']
+        self.temp_min = data['main']['temp_min']
+        self.temp_max = data['main']['temp_max']
+        self.pressure = data['main']['pressure']
+        self.humidity = data['main']['humidity']
+        self.windSpeed = data['wind']['speed']
+       
+def console_log_weather_data():
+    print('{:d}:{:02d}\n'.format(currentTime.hour, currentTime.minute))
+    print(f'{currentTime.date()} | {currentTime.time()}  | -> H:{currentTime.hour}|M:{currentTime.minute}')
+    print(f'City:{DG.cityName} | Desc:{WD.description}, CurrT:{WD.currentTemp}, FLT:{WD.feelsLikeTemp}, Range:{WD.temp_min} to {WD.temp_max}, Press:{WD.pressure}, Hum:{WD.humidity}, Wind:{WD.windSpeed}\n')
 
+def db_log_weather_data(DG,WD):
+    with UseDatabase(dbconfig) as cursor:
+        cursor.execute(_SQL,(DG.cityName,WD.description,WD.currentTemp,WD.feelsLikeTemp,WD.temp_min,WD.temp_max,WD.pressure,WD.humidity,WD.windSpeed,))
+
+DG = DataGatherer('Tczew','metric',apiKey)
 
 while True:
-    response = requests.get(fullAddress)
-    data = response.json()
-    print(type(data))
+
+    data = DG.gather_data()
+    WD = Weather(data)
     currentTime = datetime.datetime.now()
 
-    description = data['weather'][0]['description']
-    currentTemp = data['main']['temp']
-    feelsLikeTemp = data['main']['feels_like']
-    temp_min = data['main']['temp_min']
-    temp_max = data['main']['temp_max']
-    pressure = data['main']['pressure']
-    humidity = data['main']['humidity']
-    windSpeed = data['wind']['speed'] 
-
-    print(f'{currentTime} -> H:{currentTime.hour}|M:{currentTime.minute} ')
-
     if currentTime.minute == 0:
-        print('Whole Hour:{:d}:{:02d}\n'.format(currentTime.hour, currentTime.minute))
-        with open('WeatherLog.txt','a') as f:
-            f.write(f'WHOLE HOUR: {currentTime}, City:{cityName}|| Description:{description}, CurrentTemperature:{currentTemp}, FeelsLikeTemperature:{feelsLikeTemp}, Ranging between:{temp_min} to {temp_max}, Pressure:{pressure}, Humidity:{humidity}, WindSpeed:{windSpeed}\n')
+        
+        data = DG.gather_data()
+        WD = Weather(data)
+
+        db_log_weather_data(DG,WD)
+        console_log_weather_data()
     else:
-        print('{:d}:{:02d}\n'.format(currentTime.hour, currentTime.minute))
+        console_log_weather_data()
 
     time.sleep(60)
