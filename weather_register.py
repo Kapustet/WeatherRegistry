5ba8e5a84d0c37e379dd271bfc92188adf614eb8
wl <Wojciech.Langowski@gmx.com>
2021-08-27 10:07:18 +0200

    weather_register.py unnecessary loops deleted

--- weather_register.py
+++ weather_register.py
@@ -12,10 +12,19 @@ class DataGatherer:
         self.units = units
         self.api_key = api_key
     
-    def gather_data(self)->dict:
+    def get_data(self)->dict:
         full_address = f'http://api.openweathermap.org/data/2.5/weather?q={self.city_name}&units={self.units}&appid={self.api_key}'
-        data = requests.get(full_address).json()
-        return data
+        for i in range(5):
+            try:
+                
+                data = requests.get(full_address).json()
+            except ConnectionError as Argument:
+                print(f'An error occured: {Argument}\n')
+                db_log_error(Argument)
+                time.sleep(6)                   
+                continue
+            return data
+            
 
 class Weather:
     def __init__(self,data:dict):
@@ -28,7 +37,7 @@ class Weather:
         self.humidity = data['main']['humidity']
         self.wind_speed = data['wind']['speed']
        
-def console_log_weather_data():
+def console_log_weather_data(current_time,data_gatherer,weather_data):
     print('{:d}:{:02d}\n'.format(current_time.hour, current_time.minute))
     print(f'{current_time.date()} | {current_time.time()}  | -> H:{current_time.hour}|M:{current_time.minute}')
     print(f'City:{data_gatherer.city_name} | Desc:{weather_data.description}, CurrT:{weather_data.current_temp}, FLT:{weather_data.feels_like_temp}, Range:{weather_data.temp_min} to {weather_data.temp_max}, Press:{weather_data.pressure}, Hum:{weather_data.humidity}, Wind:{weather_data.wind_speed}\n')
@@ -41,36 +50,25 @@ def db_log_error(error):
     with UseDatabase(dbconfig) as cursor:
         cursor.execute(_INSERT_INTO_ERROR_LOG,(str(error),))
 
-data_gatherer = DataGatherer('Tczew','metric',api_key)
+def timed_loop():
+    iterate_data('Tczew')
+    time.sleep(60)
+    
+def iterate_data(city_name):
+    data_gatherer = DataGatherer(city_name,'metric',api_key)
+    data = data_gatherer.get_data()
+    weather_data = Weather(data)
+    current_time = datetime.datetime.now()
+       
+    if current_time.minute == 00:
+        db_log_weather_data(data_gatherer,weather_data)
+        console_log_weather_data(current_time,data_gatherer,weather_data)
+    else:
+        console_log_weather_data(current_time,data_gatherer,weather_data)
+
 
 while True:
-    for i in range(5):
-        try:
-    
-            data = data_gatherer.gather_data()
-            weather_data = Weather(data)
-        except ConnectionError as Argument:
-            print(f'An error occured: {Argument}\n')
-            db_log_error(Argument)
-            time.sleep(6)
-            continue
-        break
-    current_time = datetime.datetime.now()
+    timed_loop()
 
-    for i in range(5):   
-        try:
-            if current_time.minute == 00:
-        
-                data = data_gatherer.gather_data()
-                weather_data = Weather(data)
-                db_log_weather_data(data_gatherer,weather_data)
-                console_log_weather_data()
-            else:
-                console_log_weather_data()
-        except Exception as Argument:
-            print(f'An error occured: {Argument}')
-            db_log_error(Argument)
-            time.sleep(6)
-            continue
-        break
-    time.sleep(60)
+
+    
